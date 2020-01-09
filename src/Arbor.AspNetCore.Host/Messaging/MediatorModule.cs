6e92795ab7fea357770dd0e4d138df1b12519b1b
Niklas Lundberg <info@nlundberg.se>
2020-01-09 21:59:04 +0100

    Added MediatR module.

---
+++ src/Arbor.AspNetCore.Host/Messaging/MediatorModule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Arbor.App.Extensions;
+using Arbor.App.Extensions.Application;
+using Arbor.App.Extensions.DependencyInjection;
+using JetBrains.Annotations;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Arbor.AspNetCore.Host.Messaging
+{
+    [UsedImplicitly]
+    public class MediatorModule : IModule
+    {
+        public IServiceCollection Register(IServiceCollection builder)
+        {
+            RegisterTypes(typeof(IPipelineBehavior<,>), builder, ServiceLifetime.Singleton);
+            RegisterTypes(typeof(IRequestPostProcessor<,>), builder, ServiceLifetime.Singleton);
+            RegisterTypes(typeof(IRequestPreProcessor<>), builder, ServiceLifetime.Singleton);
+            RegisterTypes(typeof(INotificationHandler<>), builder, ServiceLifetime.Singleton);
+            RegisterTypes(typeof(IRequestHandler<>), builder, ServiceLifetime.Singleton);
+            RegisterTypes(typeof(IRequestHandler<,>), builder, ServiceLifetime.Singleton);
+
+            builder.AddSingleton<ServiceFactory>(p => p.GetService, this);
+            builder.AddSingleton<IMediator, Mediator>(this);
+
+            builder.AddSingleton(typeof(IPipelineBehavior<,>),
+                typeof(RequestPreProcessorBehavior<,>),
+                this);
+
+            builder.AddSingleton(typeof(IPipelineBehavior<,>), typeof(RequestPostProcessorBehavior<,>), this);
+
+
+            return builder;
+        }
+
+        private void RegisterTypes(
+            Type openGenericType,
+            IServiceCollection builder,
+            ServiceLifetime serviceLifetime)
+        {
+            var types = ApplicationAssemblies.FilteredAssemblies()
+                .SelectMany(a => a.GetLoadableTypes())
+                .Where(t => t.IsPublic && t.IsConcrete())
+                .Where(concreteType => concreteType.Closes(openGenericType))
+                .ToArray();
+
+            foreach (var implementationType in types)
+            {
+                builder.Add(new ExtendedServiceDescriptor(implementationType,
+                    implementationType,
+                    serviceLifetime,
+                    GetType()));
+
+                var interfaces = implementationType.GetInterfaces();
+
+                foreach (var @interface in interfaces)
+                {
+                    builder.Add(new ExtendedServiceDescriptor(@interface,
+                        context => context.GetService(implementationType),
+                        serviceLifetime,
+                        GetType()));
+                }
+            }
+        }
+    }
+}
